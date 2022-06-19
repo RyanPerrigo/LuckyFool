f7f598824e66306ea1e37f7f43eb90db39170945
Imantium <Ryan.Perrigo@gmail.com>
2022-06-19 16:27:01 -0500

    Initial Commit

---
+++ Shared/LuckyFoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuckyFoolApp.swift
+//  Shared
+//
+//  Created by Ryan Perrigo on 6/19/22.
+//
+
+import SwiftUI
+
+@main
+struct LuckyFoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
